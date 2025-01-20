18be94da5662fba00e7df04b1f585178411da35e
defaultuser0 <axolotlslmao@gmail.com>
2025-01-19 21:24:27 -0600

    Update PluginInfo.cs

--- EditYourSizeMod/PluginInfo.cs
+++ EditYourSizeMod/PluginInfo.cs
@@ -1,12 +1,9 @@
 namespace EditYourSizeMod
 {
-    /// <summary>
-    /// This class is used to provide information about your mod to BepInEx.
-    /// </summary>
     internal class PluginInfo
     {
         public const string GUID = "com.rateixvr.default.gorillatag.EditYourSizeMod";
         public const string Name = "EditYourSizeMod";
-        public const string Version = "1.0.3";
+        public const string Version = "1.0.0";
     }
 }
